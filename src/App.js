3bc6e94a382de73ccf52e36533b83f10d0e18d89
cshensql <cshencm@gmail.com>
2022-11-02 16:03:47 -0400

    first commit; basic functions and no styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const {
+  Heater1,
+  Heater2,
+  Heater3,
+  Heater4,
+  Heater6,
+  CevH2,
+  DscOh,
+  KicknHat,
+  RP4Kick,
+} = require("./drum-sounds/sounds");
+
+class DrumPad extends React.Component {
+  // props: url, id, text, playAudio
+
+  render() {
+    return (
+      <div
+        id={this.props.id}
+        className="drum-pad"
+        onClick={this.props.playAudio}
+      >
+        {this.props.text}
+        <audio
+          id={this.props.text}
+          src={this.props.url}
+          type="audio/mp3"
+          className="clip"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Your brower does not support the audio element.
+        </audio>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  state = {
+    power: true,
+    l1: [
+      "Heater-1",
+      "Heater-2",
+      "Heater-3",
+      "Heater-4",
+      "Clap",
+      "Kick-n'-Hat",
+      "Open-HH",
+      "Kick",
+      "Closed-HH",
+    ],
+    texts: ["Q", "W", "E", "A", "S", "D", "Z", "X", "C"],
+    urls: [
+      Heater1,
+      Heater2,
+      Heater3,
+      Heater4,
+      Heater6,
+      CevH2,
+      DscOh,
+      KicknHat,
+      RP4Kick,
+    ],
+  };
+
+  playAudio = (event) => {
+    //const ele = document.getElementById(this.props.text);
+    if (this.state.power) {
+      const drumPad = event.target;
+      drumPad.lastChild.play();
+      document.getElementById("display").textContent = drumPad.id;
+    }
+    //ele.play();
+  };
+
+  powerSwitch = () => {
+    document.getElementById("display").textContent = "";
+    this.setState({ power: !this.state.power });
+  };
+
+  render() {
+    const padBank = [];
+    for (let i = 0; i < 9; i++) {
+      padBank.push(
+        <DrumPad
+          id={this.state.l1[i]}
+          text={this.state.texts[i]}
+          url={this.state.urls[i]}
+          playAudio={this.playAudio}
+        />
+      );
+    }
+    return (
+      <div id="drum-machine">
+        <div id="pad-bank">{padBank}</div>
+        <div id="controls">
+          <button id="power-btn" onClick={this.powerSwitch}>
+            {this.state.power? "Power: ON": "Power: OFF"}
+          </button>
+          <p id="display"></p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/drum-sounds/sounds.js
@@ -0,0 +1,21 @@
+const Heater1 = require("./Heater-1.mp3"),
+  Heater2 = require("./Heater-2.mp3"),
+  Heater3 = require("./Heater-3.mp3"),
+  Heater4 = require("./Heater-4.mp3"),
+  Heater6 = require("./Heater-6.mp3"),
+  CevH2 = require("./Cev_H2.mp3"),
+  DscOh = require("./Dsc_Oh.mp3"),
+  KicknHat = require("./Kick_n_Hat.mp3"),
+  RP4Kick = require("./RP4_KICK_1.mp3");
+
+module.exports = {
+  Heater1,
+  Heater2,
+  Heater3,
+  Heater4,
+  Heater6,
+  CevH2,
+  DscOh,
+  KicknHat,
+  RP4Kick
+};
