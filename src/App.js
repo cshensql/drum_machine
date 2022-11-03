bb0c8a3cb00d8734884024727fde1659cf3e5b66
cshensql <cshencm@gmail.com>
2022-11-03 17:22:57 -0400

    working version with basic styles

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const {
 } = require("./drum-sounds/sounds");
 
 class DrumPad extends React.Component {
-  // props: url, id, text, playAudio
+  // props: url, id, text, playAudio, handleKeyDown
 
   render() {
     return (
@@ -39,8 +39,9 @@ class DrumPad extends React.Component {
 
 class App extends React.Component {
   state = {
-    power: true,
-    l1: [
+    power: false,
+    display: "",
+    ids: [
       "Heater-1",
       "Heater-2",
       "Heater-3",
@@ -65,41 +66,78 @@ class App extends React.Component {
     ],
   };
 
+  playDrum = (drumPad) => {
+    drumPad.lastChild.play();
+    //document.getElementById("display").textContent = drumPad.id;
+    drumPad.className += " keyDown";
+    setTimeout(()=> drumPad.className="drum-pad",100);
+    this.setState({ display: drumPad.id });
+  };
   playAudio = (event) => {
     //const ele = document.getElementById(this.props.text);
     if (this.state.power) {
       const drumPad = event.target;
-      drumPad.lastChild.play();
-      document.getElementById("display").textContent = drumPad.id;
+      this.playDrum(drumPad);
     }
     //ele.play();
   };
-
-  powerSwitch = () => {
-    document.getElementById("display").textContent = "";
-    this.setState({ power: !this.state.power });
+  handleKeyDown = (event) => {
+    //console.log(event.key);
+    if (this.state.power) {
+      const key = event.key;
+      //console.log(key, typeof key);
+      if (key.match(/^[qweasdzxc]$/i)) {
+        //console.log("succeed!");
+        const index = this.state.texts.indexOf(key.toUpperCase());
+        const drumPad = document.getElementById(this.state.ids[index]);
+        drumPad.click();
+        /*this.playDrum(drumPad);
+        drumPad.className += " keyDown";*/
+      }
+    }
   };
-
+  /*handleKeyUp = (event) =>{
+    document.getElementById(this.state.display).className = "drum-pad";
+  };*/
+  powerSwitch = () => {
+    //document.getElementById("display").textContent = "";
+    this.setState({ power: !this.state.power, display: "" });
+  };
+  componentDidMount() {
+    document.getElementById("drum-machine").focus();
+  }
+  /*componentDidUpdate() {
+    document.getElementById("drum-machine").focus();
+  }*/
   render() {
     const padBank = [];
     for (let i = 0; i < 9; i++) {
       padBank.push(
         <DrumPad
-          id={this.state.l1[i]}
+          id={this.state.ids[i]}
+          key= {this.state.ids[i]}
           text={this.state.texts[i]}
           url={this.state.urls[i]}
           playAudio={this.playAudio}
+          //handleKeyDown={this.handleKeyDown}
         />
       );
     }
+    console.log("the focused element:", document.activeElement);
     return (
-      <div id="drum-machine">
+      <div
+        id="drum-machine" tabIndex="0"
+        onKeyDown={this.handleKeyDown}
+        onKeyUp={this.handleKeyUp}
+      >
         <div id="pad-bank">{padBank}</div>
         <div id="controls">
-          <button id="power-btn" onClick={this.powerSwitch}>
-            {this.state.power? "Power: ON": "Power: OFF"}
-          </button>
-          <p id="display"></p>
+          <span>{this.state.power ? "Power: ON" : "Power: OFF"}</span>
+          <label className="switch">
+            <input type="checkbox" onClick={this.powerSwitch} />
+            <span className="slider"></span>
+          </label>
+          <p id="display">{this.state.display}</p>
         </div>
       </div>
     );
